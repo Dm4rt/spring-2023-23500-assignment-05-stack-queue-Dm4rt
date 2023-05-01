366c210098371fa5f69e5516b5e26c9133b678ac
dm4rt <danmartinez2004@gmail.com>
2023-05-01 18:53:49 -0400

    have to fix bugs on queue

---
+++ queue.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "queue.h"
+
+
+queue::queue(){
+	MAX_SIZE=10;
+	head=0;
+	tail=0;
+}
+
+queue::queue(int size){
+	MAX_SIZE=size;
+	head=0-;
+	tail=0;
+}
+
+queue::~queue(){
+	line.clear();
+	head=0;
+	tail=0;
+}
+
+void queue::enqueue(std::string data){
+	if(is_empty){
+		data=
+	}
+}
+
+std::string queue::dequeue(){
+	
+}
+
+std::string queue::front(){
+	
+}
+
+bool is_empty(){
+	
+}
+
+/*
+		std::string front();
+		bool is_empty();
+		bool is_full();
+};
+*/
---
+++ queue.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class queue{
+	private:
+		vector<std::string> line;
+		const int MAX_SIZE;
+		int head, tail;
+		
+	public:
+	
+		queue();
+		queue(int size);
+		~queue();
+		void enqueue(std::string data);
+		std::string dequeue();
+		std::string front();
+		bool is_empty();
+		bool is_full();
+};
